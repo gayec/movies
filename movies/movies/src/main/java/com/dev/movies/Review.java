ac8bcf19d1ba716bf3466a2fc60fdb31a48b395b
gayec <gayeeceliik@gmail.com>
2024-05-24 00:24:20 +0300

    controllers and services are added

--- movies/movies/src/main/java/com/dev/movies/Review.java
+++ movies/movies/src/main/java/com/dev/movies/Review.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "reviews")
@@ -12,6 +13,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 public class Review {
 
+    @Id
     private ObjectId id;
     private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
 }
